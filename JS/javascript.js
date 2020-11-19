28acf0ce809de4dd0b970dee64883be9c6da6396
ianwrig <you@example.com>
2020-11-19 14:30:35 -0500

    Nav Bar Updates

--- JS/javascript.js
+++ JS/javascript.js
@@ -1,4 +1,4 @@
-<script>
+
 function myFunction() {
   var x = document.getElementById("myTopnav");
   if (x.className === "topnav") {
@@ -7,4 +7,4 @@ function myFunction() {
     x.className = "topnav";
   }
 }
-</script>
+
