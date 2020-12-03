405305d188106de6897b941ed2475c539419a871
ianwrig <you@example.com>
2020-12-03 10:51:03 -0500

    Contact us updates

--- JS/javascript.js
+++ JS/javascript.js
@@ -30,3 +30,10 @@ function openNav() {
     x.className = x.className.replace(" w3-show", "");
   }
 }
+
+function ValidateForm(frm) {
+  if (frm.Name.value == "") { alert('Name is required.'); frm.Name.focus(); return false; }
+  if (frm.FromEmailAddress.value == "") { alert('Email address is required.'); frm.FromEmailAddress.focus(); return false; }
+  if (frm.FromEmailAddress.value.indexOf("@") < 1 || frm.FromEmailAddress.value.indexOf(".") < 1) { alert('Please enter a valid email address.'); frm.FromEmailAddress.focus(); return false; }
+  if (frm.Comments.value == "") { alert('Please enter comments or questions.'); frm.Comments.focus(); return false; }
+  return true; }
